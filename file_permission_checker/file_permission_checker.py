3dbfe04a690cb2b2934c3c3902cc1ff13817238d
saachiaggarwal7 <saachiaggarwal7@gmail.com>
2026-06-11 14:41:12 +0530

    Update file_permission_checker.py

--- file_permission_checker/file_permission_checker.py
+++ file_permission_checker/file_permission_checker.py
@@ -1,4 +1,5 @@
 import os
+import stat
 path=input("enter path:")
 if os.path.exists(path):
     if os.path.isfile(path):
@@ -10,5 +11,8 @@ if os.path.exists(path):
     print("Execute:     ",os.access(path,os.X_OK))
     perm=oct(os.stat(path).st_mode)[-3:]
     print("Permissions: ",perm)
+    print("Symbolic:",stat.filemode(os.stat(path).st_mode))
+    if perm in ["777","767","666"]:
+        print("[WARNING] File is world-writable and may pose a security risk")
 else:
     print("invalid path")
